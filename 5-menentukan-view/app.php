1c9becdc3ffd70a40e139100d359cd1d8c9db965
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-11 16:57:14 +0700

    Menentukan View, Studi Kasus PHP Dasar Aplikasi Todolist

---
+++ 5-menentukan-view/app.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "5-menentukan-view/model-todo/todolist.php";
+require_once "5-menentukan-view/business-logic/showtodolist.php";
+require_once "5-menentukan-view/business-logic/addtodolist.php";
+require_once "5-menentukan-view/business-logic/removetodolist.php";
+require_once "5-menentukan-view/view/viewShowToDoList.php";
+require_once "5-menentukan-view/view/viewAddToDoList.php";
+require_once "5-menentukan-view/view/viewRemoveToDoList.php";
+
+
+echo "Aplikasi ToDoList" . PHP_EOL;
